44f0dd6ef8bf45d64f4e307a879a2a4e2e69a5e8
lihongzhan <1028664523@qq.com>
2023-03-22 15:17:07 +0800

    提交日志

---
+++ Framework/HZRecorder.framework/Headers/HZRecorder.h
@@ -0,0 +1,18 @@
+//
+//  HZRecorder.h
+//  HZRecorder
+//
+//  Created by 李洪战 on 2023/3/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HZRecorder.
+FOUNDATION_EXPORT double HZRecorderVersionNumber;
+
+//! Project version string for HZRecorder.
+FOUNDATION_EXPORT const unsigned char HZRecorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HZRecorder/PublicHeader.h>
+
+
